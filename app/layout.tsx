d65dc06c4e18f2842fbc54136d7144cc09ca05e5
Rafael Bocsa <mrraf995@gmail.com>
2025-01-13 20:29:54 -0500

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 const inter = Inter({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
         {/* More components under here */}
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
