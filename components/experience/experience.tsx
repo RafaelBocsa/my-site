9cc7802c72f806f7716792e3f0816db3942ae3c2
Rafael Bocsa <mrraf995@gmail.com>
2025-03-28 16:10:59 -0400

    updated experience

--- components/experience/experience.tsx
+++ components/experience/experience.tsx
@@ -13,36 +13,37 @@ const experience = () => {
           company="HIEC"
           position="Web Developer"
           location="Burlington, ON"
-          description="
-          Developed/designed web components in Wordpress using Elementor via collaboration with Marketing, Finance
-          and HR teams. Created custom JS and JQUERY code for specific web elements to enhance visual design, backend functionality
-          and site responsiveness. Redesigned policy page and implemented user feedback while ensuring safety and security."
+          description="Created custom JS and JQUERY code for specific web elements, enhancing visual design and backend
+          functionalities. Spearheaded the redesign of a new policy page and user feedback function, prioritizing safety and security.
+          Initiated major front and backend changes to site components, optimizing space usage and page redirects. Collaborated in pair programming to debug code, optimize routes and troubleshoot api calls in a RubyOnRails
+          environment for better user flows."
         ></Exp_card>
         <Exp_card
           year="SEPT — DEC 2022"
           company="Government of Ontario"
-          position="Junior Full-Stack Developer"
+          position="Software Developer Intern"
           location="St. Catharines, ON"
-          description="Researched new and trending software’s and technologies that could benefit the government to push the digital
-        service transformation agenda for our clients. Implemented a proof of concept lab report on web development best practices by researching and learning emerging
-        technologies such as OutSystems, Flutter/Dart, Firebase, PWA.
+          description="Accomplished a proof of concept lab report on web development best practices to over 30 public servants by
+          utilizing emerging technologies such as OutSystems, Flutter/Dart, Firebase, and PWA. Developed an automated attendance checker, reducing manual attendance monitoring efforts by 60% and providing
+          real-time updates via PowerApps. Explored new software and technologies to drive digital service transformation for clients by investigation creative
+          digital solutions, delving into complex problems to derive insights, prototype, and validate with end users.
         "
         ></Exp_card>
         <Exp_card
           year="JAN — APR 2022"
           company="Civiconnect"
-          position="Junior Full-Stack Developer"
+          position="Software Developer Intern"
           location="Beamsvile, ON"
-          description="Worked in a dynamic team environment of 3 to support front-end development and built back-end web
-        functionalities for local businesses using Python, Django, PostgreSQL and AWS. Worked to suit clients’ needs under tight time constraints, troubleshoot, debug, support web dev teams, maintain
-        web services, and presented final product to clients showcasing all back end admin functionalities.
+          description="Developed and deployed end-to-end websites for local businesses utilizing Django, PostgreSQL, and AWS.
+           Engineered backend services with Django to facilitate 50% faster content updates through the admin dashboard.
+           Demonstrated functional backend features punctually, ensuring 100% client satisfaction and addressing needs within tight deadlines.
         "
         ></Exp_card>
-        <div className="py-10">
+        {/* <div className="py-10">
           <a href="public\RafaelBocsa_Resume.pdf" download target="_blank">
             View Full Résumé →
           </a>
-        </div>
+        </div> */}
       </div>
     </div>
   );
