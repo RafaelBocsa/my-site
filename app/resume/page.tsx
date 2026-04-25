37b363fe25525939b9d886b73210dc74100705da
RafaelBocsa <mrraf995@gmail.com>
2026-04-25 02:17:22 -0400

    Improved mobile resume view, updated packages

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+"use client";
+import dynamic from "next/dynamic";
 
-const ResumeView = () => {
+// import ResumeViewer from "@/components/resume_viewer/resumeViewer";
+
+const ResumeViewer = dynamic(
+  () => import("../../components/resume_viewer/resumeViewer"),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="flex h-screen items-center justify-center">
+        <p className="text-lg text-white">Loading Resume...</p>
+      </div>
+    ),
+  },
+);
+
+export default function ResumeView() {
   const pdfURL = "/docs/RafaelBocsa_2026.pdf";
 
   return (
-    <div className="w-screen h-screen overflow-auto bg-none z-10">
-      <embed src={pdfURL} type="application/pdf" className="w-full h-full" />
+    <div style={{ height: "100vh" }}>
+      <ResumeViewer file="/docs/RafaelBocsa_2026.pdf" />
     </div>
   );
-};
-
-export default ResumeView;
+}
---
+++ components/resume_viewer/resumeViewer.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url,
+).toString();
+
+export default function resumeViewer({ file }: { file: string }) {
+  const [containerWidth, setContainerWidth] = useState<number>(800);
+
+  useEffect(() => {
+    const updateWidth = () => {
+      const width = window.innerWidth;
+
+      if (width < 640) {
+        setContainerWidth(width - 32);
+      } else if (width < 1024) {
+        setContainerWidth(Math.min(width - 64, 700));
+      } else {
+        setContainerWidth(850);
+      }
+    };
+
+    updateWidth();
+    window.addEventListener("resize", updateWidth);
+
+    return () => window.removeEventListener("resize", updateWidth);
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-full bg-none overflow-hidden">
+        <Document
+          file={file}
+          error={
+            <div className="p-8 text-center text-red-500">
+              Failed to load PDF.
+            </div>
+          }
+        >
+          <div className=" flex justify-center">
+            <Page
+              pageNumber={1}
+              width={containerWidth}
+              renderAnnotationLayer={true}
+              renderTextLayer={true}
+              className="shadow-md"
+            />
+          </div>
+        </Document>
+      </div>
+    </div>
+  );
+}
