507b1fea77e448703adf3f553bd1df69b269e7b6
RafaelBocsa <mrraf995@gmail.com>
2026-04-24 20:51:14 -0400

    resume style simplified

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,14 +4,8 @@ const ResumeView = () => {
   const pdfURL = "/docs/RafaelBocsa_2026.pdf";
 
   return (
-    <div style={{ height: "100vh", overflow: "hidden" }}>
-      <embed
-        src={pdfURL}
-        type="application/pdf"
-        width="100%"
-        height="100%"
-        style={{ border: "none" }}
-      />
+    <div className="w-screen h-screen overflow-auto bg-none z-10">
+      <embed src={pdfURL} type="application/pdf" className="w-full h-full" />
     </div>
   );
 };
